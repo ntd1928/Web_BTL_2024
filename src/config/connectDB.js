5e796c82f68554d5f88b289aa5a7f3d766f48752
ntd1407 <ndat526686@gmail.com>
2024-12-16 17:18:57 +0700

    ok

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,27 +1,17 @@
-const { Sequelize } = require('sequelize');
-
-// Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('postgres', 'postgres', 'Ndat.0362258973', {
-    host: 'db.snotzdowzfkjngwcooko.supabase.co',
-    port: 5432,
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // SSL từ Supabase
+            },
         },
-    },
-    logging: false,
-    useIPv6: false,
-});
-
-let connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        logging: false, // Tắt log
     }
-}
-
-module.exports = connectDB;
+);
