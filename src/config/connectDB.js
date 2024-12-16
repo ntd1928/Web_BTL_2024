2fa80dd19bc6ed218c220443a75a58ab21e255d7
ntd1407 <ndat526686@gmail.com>
2024-12-16 17:26:15 +0700

    ok

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,18 +1,25 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false, // SSL từ Supabase
-            },
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
         },
-        logging: false, // Tắt log
+    },
+    logging: false,
+});
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
     }
-);
+};
+
+module.exports = connectDB;
